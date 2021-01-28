04d9da3fea927d80e304e230651080f92e4192e7
hiskim1 <tomm1203@gist.ac.kr>
2021-01-28 15:23:30 +0900

    mining for bitcoin-ecc

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+int main(){
+    system("bitcoind -txindex -daemon");
+    sleep(5);
+    cout<<endl<<"[ your address ]"<<endl;
+    system("bitcoin-cli getnewaddress");
+    string addr;
+    cout<<"\n\nEnter the address to get minted coins"<<endl;
+    cin>>addr;
+    string s_command = "bitcoin-cli generatetoaddress 1 " + addr;
+    const char *c_command = s_command.c_str();
+    while(true){
+        system(c_command);
+    }
+    return 0;
+}
